5f14f6d45b177b2b93b47c344381e0014326975a
Cheese <597461996@qq.com>
2020-08-30 20:24:07 +0800

    Update configure.sh
    "alterId": 4
    "path": "/voicemail"

--- configure.sh
+++ configure.sh
@@ -22,14 +22,18 @@ cat << EOF > /usr/local/etc/v2ray/config.json
                 "clients": [
                     {
                         "id": "$UUID",
-                        "alterId": 64
+                        "alterId": 4
                     }
                 ],
                 "disableInsecureEncryption": true
             },
-            "streamSettings": {
-                "network": "ws"
-            }
+    "streamSettings": {
+      "network": "ws",
+      "security": "auto",
+      "wsSettings": {
+        "path": "/voicemail",
+      }
+    }
         }
     ],
     "outbounds": [
